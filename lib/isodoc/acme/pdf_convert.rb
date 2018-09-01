027f26c18b07692033480d6aa4abe0e3a4e8660a
Nick Nicholas <opoudjis@optusnet.com.au>
2018-09-01 22:32:16 +1000

    no anchor icons in pdf output

--- lib/isodoc/acme/pdf_convert.rb
+++ lib/isodoc/acme/pdf_convert.rb
@@ -28,7 +28,7 @@ module IsoDoc
           htmlstylesheet: html_doc_path("htmlstyle.scss"),
           htmlcoverpage: html_doc_path("html_acme_titlepage.html"),
           htmlintropage: html_doc_path("html_acme_intro.html"),
-          scripts: html_doc_path("scripts.html"),
+          scripts: html_doc_path("scripts.pdf.html"),
         }
       end
 
