0a4119b55d44b424494b8187161a855d61aed909
Nick Nicholas <opoudjis@optusnet.com.au>
2018-10-14 23:33:45 +1100

    parse annex titles; https://github.com/riboseinc/isodoc/issues/76

--- lib/isodoc/acme/html_convert.rb
+++ lib/isodoc/acme/html_convert.rb
@@ -33,12 +33,12 @@ module IsoDoc
         }
       end
 
-        def metadata_init(lang, script, labels)
-          @meta = Metadata.new(lang, script, labels)
-        end
+      def metadata_init(lang, script, labels)
+        @meta = Metadata.new(lang, script, labels)
+      end
 
-        def html_head
-          <<~HEAD.freeze
+      def html_head
+        <<~HEAD.freeze
           <title>{{ doctitle }}</title>
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
@@ -52,148 +52,90 @@ module IsoDoc
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/solid.css" integrity="sha384-v2Tw72dyUXeU3y4aM2Y0tBJQkGfplr39mxZqlTBDUZAb9BGoC40+rdFCG0m10lXk" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/fontawesome.css" integrity="sha384-q3jl8XQu1OpdLgGFvNRnPdj5VIlCvgsDQTQB6owSOHWlAurxul7f+JpUOVdAiJ5P" crossorigin="anonymous">
     <style class="anchorjs"></style>
-          HEAD
-        end
-
-        def make_body(xml, docxml)
-          body_attr = { lang: "EN-US", link: "blue", vlink: "#954F72", "xml:lang": "EN-US", class: "container" }
-          xml.body **body_attr do |body|
-            make_body1(body, docxml)
-            make_body2(body, docxml)
-            make_body3(body, docxml)
-          end
-        end
-
-        def html_toc(docxml)
-          docxml
-        end
-
-        def annex_name(annex, name, div)
-          div.h1 **{ class: "Annex" } do |t|
-            t << "#{get_anchors[annex['id']][:label]} "
-            t << "<b>#{name.text}</b>"
-          end
-        end
-
-        def annex_name_lbl(clause, num)
-          obl = l10n("(#{@inform_annex_lbl})")
-          obl = l10n("(#{@norm_annex_lbl})") if clause["obligation"] == "normative"
-          l10n("<b>#{@annex_lbl} #{num}</b> #{obl}")
-        end
-
-        def pre_parse(node, out)
-          out.pre node.text # content.gsub(/</, "&lt;").gsub(/>/, "&gt;")
-        end
-
-        def term_defs_boilerplate(div, source, term, preface)
-          if source.empty? && term.nil?
-            div << @no_terms_boilerplate
-          else
-            div << term_defs_boilerplate_cont(source, term)
-          end
-        end
-
-        def i18n_init(lang, script)
-          super
-          @annex_lbl = "Appendix"
-        end
-
-        def error_parse(node, out)
-          # catch elements not defined in ISO
-          case node.name
-          when "pre"
-            pre_parse(node, out)
-          when "keyword"
-            out.span node.text, **{ class: "keyword" }
-          else
-            super
-          end
-        end
-
-        def fileloc(loc)
-          File.join(File.dirname(__FILE__), loc)
-        end
-
-        def cleanup(docxml)
-          super
-          term_cleanup(docxml)
-        end
-
-        def term_cleanup(docxml)
-          docxml.xpath("//p[@class = 'Terms']").each do |d|
-            h2 = d.at("./preceding-sibling::*[@class = 'TermNum'][1]")
-            h2.add_child("&nbsp;")
-            h2.add_child(d.remove)
-          end
-          docxml
-        end
-
-        def info(isoxml, out)
-          @meta.security isoxml, out
-          super
-        end
-
-        def annex_name(annex, name, div)
-          div.h1 **{ class: "Annex" } do |t|
-            t << "#{get_anchors[annex['id']][:label]} "
-            t << "<b>#{name.text}</b>"
-          end
-        end
-
-        def annex_name_lbl(clause, num)
-          obl = l10n("(#{@inform_annex_lbl})")
-          obl = l10n("(#{@norm_annex_lbl})") if clause["obligation"] == "normative"
-          l10n("<b>#{@annex_lbl} #{num}</b> #{obl}")
-        end
-
-        def pre_parse(node, out)
-          out.pre node.text # content.gsub(/</, "&lt;").gsub(/>/, "&gt;")
-        end
-
-        def term_defs_boilerplate(div, source, term, preface)
-          if source.empty? && term.nil?
-            div << @no_terms_boilerplate
-          else
-            div << term_defs_boilerplate_cont(source, term)
-          end
-        end
-
-        def i18n_init(lang, script)
-          super
-          @annex_lbl = "Appendix"
-        end
-
-        def error_parse(node, out)
-          # catch elements not defined in ISO
-          case node.name
-          when "pre"
-            pre_parse(node, out)
-          when "keyword"
-            out.span node.text, **{ class: "keyword" }
-          else
-            super
-          end
-        end
-
-        def fileloc(loc)
-          File.join(File.dirname(__FILE__), loc)
-        end
-
-        def cleanup(docxml)
-          super
-          term_cleanup(docxml)
-        end
-
-        def term_cleanup(docxml)
-          docxml.xpath("//p[@class = 'Terms']").each do |d|
-            h2 = d.at("./preceding-sibling::*[@class = 'TermNum'][1]")
-            h2.add_child("&nbsp;")
-            h2.add_child(d.remove)
-          end
-          docxml
-        end
-
+        HEAD
       end
+
+      def make_body(xml, docxml)
+        body_attr = { lang: "EN-US", link: "blue", vlink: "#954F72", "xml:lang": "EN-US", class: "container" }
+        xml.body **body_attr do |body|
+          make_body1(body, docxml)
+          make_body2(body, docxml)
+          make_body3(body, docxml)
+        end
+      end
+
+      def html_toc(docxml)
+        docxml
+      end
+
+      def annex_name(annex, name, div)
+        div.h1 **{ class: "Annex" } do |t|
+          t << "#{get_anchors[annex['id']][:label]} "
+          t.b do |b|
+            name&.children&.each { |c2| parse(c2, b) }
+          end
+        end
+      end
+
+      def annex_name_lbl(clause, num)
+        obl = l10n("(#{@inform_annex_lbl})")
+        obl = l10n("(#{@norm_annex_lbl})") if clause["obligation"] == "normative"
+        l10n("<b>#{@annex_lbl} #{num}</b> #{obl}")
+      end
+
+      def pre_parse(node, out)
+        out.pre node.text # content.gsub(/</, "&lt;").gsub(/>/, "&gt;")
+      end
+
+      def term_defs_boilerplate(div, source, term, preface)
+        if source.empty? && term.nil?
+          div << @no_terms_boilerplate
+        else
+          div << term_defs_boilerplate_cont(source, term)
+        end
+      end
+
+      def i18n_init(lang, script)
+        super
+        @annex_lbl = "Appendix"
+      end
+
+      def error_parse(node, out)
+        # catch elements not defined in ISO
+        case node.name
+        when "pre"
+          pre_parse(node, out)
+        when "keyword"
+          out.span node.text, **{ class: "keyword" }
+        else
+          super
+        end
+      end
+
+      def fileloc(loc)
+        File.join(File.dirname(__FILE__), loc)
+      end
+
+      def cleanup(docxml)
+        super
+        term_cleanup(docxml)
+      end
+
+      def term_cleanup(docxml)
+        docxml.xpath("//p[@class = 'Terms']").each do |d|
+          h2 = d.at("./preceding-sibling::*[@class = 'TermNum'][1]")
+          h2.add_child("&nbsp;")
+          h2.add_child(d.remove)
+        end
+        docxml
+      end
+
+      def info(isoxml, out)
+        @meta.security isoxml, out
+        super
+      end
+
     end
   end
+end
 
