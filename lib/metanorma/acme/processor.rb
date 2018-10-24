8414bd8ae84efb6debc3b4dc5c1f432a631c1bb3
Nick Nicholas <opoudjis@optusnet.com.au>
2018-10-24 19:44:47 +1100

    propagate filename into asciidoctor processing; https://github.com/riboseinc/metanorma-cli/issues/9

--- lib/metanorma/acme/processor.rb
+++ lib/metanorma/acme/processor.rb
@@ -22,8 +22,8 @@ module Metanorma
         "Metanorma::Acme #{Metanorma::Acme::VERSION}"
       end
 
-      def input_to_isodoc(file)
-        Metanorma::Input::Asciidoc.new.process(file, @asciidoctor_backend)
+      def input_to_isodoc(file, filename)
+        Metanorma::Input::Asciidoc.new.process(file, filename, @asciidoctor_backend)
       end
 
       def extract_options(file)
--- spec/asciidoctor/base_spec.rb
+++ spec/asciidoctor/base_spec.rb
@@ -4,7 +4,6 @@ require "fileutils"
 RSpec.describe Asciidoctor::Acme do
   it "generates output for the Rice document" do
     FileUtils.rm_rf %w(spec/examples/rfc6350.doc spec/examples/rfc6350.html spec/examples/rfc6350.pdf)
-    #system "cd spec/examples; asciidoctor --trace -b acme -r 'metanorma-acme' rfc6350.adoc; cd ../.."
     FileUtils.cd "spec/examples"
     Asciidoctor.convert_file "rfc6350.adoc", {:attributes=>{"backend"=>"acme"}, :safe=>0, :header_footer=>true, :requires=>["metanorma-acme"], :failure_level=>4, :mkdirs=>true, :to_file=>nil}
     FileUtils.cd "../.."
