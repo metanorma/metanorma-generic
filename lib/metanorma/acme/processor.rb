eeb7cc60d3c7beb50e9e6f6fa3d3ab9d36b3f027
Nick Nicholas <opoudjis@optusnet.com.au>
2018-11-21 10:31:55 +1000

    redundant code

--- lib/metanorma/acme/processor.rb
+++ lib/metanorma/acme/processor.rb
@@ -26,35 +26,6 @@ module Metanorma
         Metanorma::Input::Asciidoc.new.process(file, filename, @asciidoctor_backend)
       end
 
-      def extract_options(file)
-        head = file.sub(/\n\n.*$/m, "\n")
-        /\n:htmlstylesheet: (?<htmlstylesheet>[^\n]+)\n/ =~ head
-        /\n:htmlcoverpage: (?<htmlcoverpage>[^\n]+)\n/ =~ head
-        /\n:htmlintropage: (?<htmlintropage>[^\n]+)\n/ =~ head
-        /\n:scripts: (?<scripts>[^\n]+)\n/ =~ head
-        /\n:wordstylesheet: (?<wordstylesheet>[^\n]+)\n/ =~ head
-        /\n:standardstylesheet: (?<standardstylesheet>[^\n]+)\n/ =~ head
-        /\n:header: (?<header>[^\n]+)\n/ =~ head
-        /\n:wordcoverpage: (?<wordcoverpage>[^\n]+)\n/ =~ head
-        /\n:wordintropage: (?<wordintropage>[^\n]+)\n/ =~ head
-        /\n:ulstyle: (?<ulstyle>[^\n]+)\n/ =~ head
-        /\n:olstyle: (?<olstyle>[^\n]+)\n/ =~ head
-        new_options = {
-          htmlstylesheet: defined?(htmlstylesheet) ? htmlstylesheet : nil,
-          htmlcoverpage: defined?(htmlcoverpage) ? htmlcoverpage : nil,
-          htmlintropage: defined?(htmlintropage) ? htmlintropage : nil,
-          scripts: defined?(scripts) ? scripts : nil,
-          wordstylesheet: defined?(wordstylesheet) ? wordstylesheet : nil,
-          standardstylesheet: defined?(standardstylesheet) ? standardstylesheet : nil,
-          header: defined?(header) ? header : nil,
-          wordcoverpage: defined?(wordcoverpage) ? wordcoverpage : nil,
-          wordintropage: defined?(wordintropage) ? wordintropage : nil,
-          ulstyle: defined?(ulstyle) ? ulstyle : nil,
-          olstyle: defined?(olstyle) ? olstyle : nil,
-        }.reject { |_, val| val.nil? }
-        super.merge(new_options)
-      end
-
       def output(isodoc_node, outname, format, options={})
         case format
         when :html
