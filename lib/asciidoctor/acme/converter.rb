f083b597157fe359335a12c289a36505ed8a2ab2
Nick Nicholas <opoudjis@optusnet.com.au>
2018-11-21 11:39:53 +1000

    remove JS-based PDF conversion

--- lib/asciidoctor/acme/converter.rb
+++ lib/asciidoctor/acme/converter.rb
@@ -104,13 +104,6 @@ module Asciidoctor
         d
       end
 
-      def pdf_convert(filename)
-        url = "#{Dir.pwd}/#{filename}.html"
-        pdfjs = File.join(File.dirname(__FILE__), 'pdf.js')
-        system "export NODE_PATH=$(npm root --quiet -g);
-                node #{pdfjs} file://#{url} #{filename}.pdf"
-      end
-
       def document(node)
         init(node)
         ret1 = makexml(node)
@@ -159,6 +152,10 @@ module Asciidoctor
 
       def html_converter(node)
         IsoDoc::Acme::HtmlConvert.new(html_extract_attributes(node))
+      end
+
+      def pdf_converter(node)
+        IsoDoc::Acme::PdfConvert.new(html_extract_attributes(node))
       end
 
       def word_converter(node)
