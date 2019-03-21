12a8ccaeafad30eb29eb298b16d4ce4d8d868fcb
Nick Nicholas <opoudjis@optusnet.com.au>
2019-03-21 23:32:30 +1100

    conditional validation; https://github.com/metanorma/metanorma-standoc/issues/44

--- lib/asciidoctor/acme/converter.rb
+++ lib/asciidoctor/acme/converter.rb
@@ -90,7 +90,7 @@ module Asciidoctor
         result << "</acme-standard>"
         result = textcleanup(result)
         ret1 = cleanup(Nokogiri::XML(result))
-        validate(ret1)
+        validate(ret1) unless @novalid
         ret1.root.add_namespace(nil, Metanorma::Acme::DOCUMENT_NAMESPACE)
         ret1
       end
