80153a2db911a5e13d6e5f2d2cbe650582f59f91
Nick Nicholas <opoudjis@gmail.com>
2020-05-01 01:41:44 +1000

    no-pdf parsing

--- lib/asciidoctor/generic/converter.rb
+++ lib/asciidoctor/generic/converter.rb
@@ -139,7 +139,7 @@ module Asciidoctor
           File.open(filename, "w") { |f| f.write(ret) }
           html_converter(node).convert filename unless node.attr("nodoc")
           word_converter(node).convert filename unless node.attr("nodoc")
-          pdf_converter(node).convert filename unless node.attr("nodoc")
+          pdf_converter(node)&.convert filename unless node.attr("nodoc")
         end
         @log.write(@localdir + @filename + ".err") unless @novalid
         @files_to_delete.each { |f| FileUtils.rm f }
