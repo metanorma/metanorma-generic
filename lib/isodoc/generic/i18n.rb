af7045316646d3c12a7584507ab95ed540385794
Nick Nicholas <opoudjis@gmail.com>
2020-09-23 00:39:05 +1000

    i18n inheritance fix

--- lib/isodoc/generic/i18n.rb
+++ lib/isodoc/generic/i18n.rb
@@ -3,6 +3,14 @@ require_relative "utils"
 module IsoDoc
   module Generic
     class I18n < IsoDoc::I18n
+      class << self
+        attr_accessor :_file
+      end
+
+      def self.inherited( k )
+        k._file = caller_locations.first.absolute_path
+      end
+
       def load_yaml1(lang, script)
         return super unless configuration.i18nyaml
         file = configuration.i18nyaml.is_a?(Hash) ?
