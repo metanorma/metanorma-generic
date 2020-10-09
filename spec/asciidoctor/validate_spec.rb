d76fc3f27d56221a118f1c3aaeab4c57d65007fb
Тараскин Михаил <MITaraskin@domclick.ru>
2020-10-09 17:03:11 +0300

    metanorma/metanorma-standoc#225 add error file generation test

---
+++ spec/asciidoctor/validate_spec.rb
@@ -0,0 +1,24 @@
+require "spec_helper"
+
+RSpec.describe Asciidoctor::Generic do
+  context "when xref_error.adoc compilation" do
+    around do |example|
+      FileUtils.rm_f "spec/assets/xref_error.err"
+      example.run
+      Dir["spec/assets/xref_error*"].each do |file|
+        next if file.match?(/adoc$/)
+
+        FileUtils.rm_f(file)
+      end
+    end
+
+    it "generates error file" do
+      expect do
+        Metanorma::Compile
+          .new
+          .compile("spec/assets/xref_error.adoc", type: "generic")
+      end.to(change { File.exist?("spec/assets/xref_error.err") }
+              .from(false).to(true))
+    end
+  end
+end
