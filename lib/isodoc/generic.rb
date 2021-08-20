9799a544fe91094d21eb5fa120e4442de20e1d79
Nick Nicholas <opoudjis@gmail.com>
2021-08-20 22:04:20 +1000

    remove pdf functionality: https://github.com/metanorma/metanorma-standoc/issues/513

--- lib/isodoc/generic.rb
+++ lib/isodoc/generic.rb
@@ -1,6 +1,5 @@
 require "isodoc/generic/metadata"
 require "isodoc/generic/html_convert"
-require "isodoc/generic/pdf_convert"
 require "isodoc/generic/word_convert"
 require "isodoc/generic/presentation_xml_convert"
 
