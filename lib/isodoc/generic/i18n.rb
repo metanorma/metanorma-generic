42a14274242a892cc8b08de02e03fb0e6c3a10bf
Nick Nicholas <opoudjis@gmail.com>
2022-05-31 20:27:17 +1000

    list captions: https://github.com/metanorma/metanorma-iso/issues/663

--- lib/isodoc/generic/i18n.rb
+++ lib/isodoc/generic/i18n.rb
@@ -7,17 +7,22 @@ module IsoDoc
         attr_accessor :_file
       end
 
-      def self.inherited( k )
-        k._file = caller_locations.first.absolute_path
+      def self.inherited(k)
+        k._file = caller_locations(1..1).first.absolute_path
       end
 
       def load_yaml1(lang, script)
         return super unless configuration.i18nyaml
-        file = configuration.i18nyaml.is_a?(Hash) ?
-          configuration.i18nyaml[lang] : configuration.i18nyaml
+
+        file = if configuration.i18nyaml.is_a?(Hash)
+                 configuration.i18nyaml[lang]
+               else
+                 configuration.i18nyaml
+               end
         return super if file.nil?
+
         y = YAML.load_file(baselocation(file))
-        super.merge(y)
+        super.deep_merge(y)
       end
 
       include Utils
