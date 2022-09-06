5917e83a817643097bd668acc77896a1d7ad2ec9
Nick Nicholas <opoudjis@gmail.com>
2022-09-06 23:53:53 +1000

    version bump

--- lib/metanorma-generic.rb
+++ lib/metanorma-generic.rb
@@ -2,7 +2,8 @@ require "metanorma/generic"
 require "asciidoctor"
 require "metanorma/generic/converter"
 require "isodoc/generic"
+require "metanorma"
 
-if defined? Metanorma
+if defined? Metanorma::Registry
   Metanorma::Registry.instance.register(Metanorma::Generic::Processor)
 end
