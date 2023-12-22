f8afaea3ad7e5dffeb059befd7914b718d586867
Nick Nicholas <opoudjis@gmail.com>
2023-12-22 23:15:20 +1100

    rspec

--- spec/metanorma/base_spec.rb
+++ spec/metanorma/base_spec.rb
@@ -375,7 +375,7 @@ RSpec.describe Metanorma::Generic do
       .to be_equivalent_to xmlpp(output)
   end
 
-    context "with configuration options" do
+  context "with configuration options" do
     subject(:convert) do
       xmlpp(Asciidoctor.convert(input, *OPTIONS))
     end
@@ -427,7 +427,7 @@ RSpec.describe Metanorma::Generic do
         { "en" => "spec/fixtures/metanorma/boilerplate.xml" }
       end
 
-      it "uses configuration options for organization and namespace" do
+      before do
         Metanorma::Generic.configure do |config|
           config.organization_name_short = organization_name_short
           config.organization_name_long = organization_name_long
@@ -447,7 +447,9 @@ RSpec.describe Metanorma::Generic do
           config.i18nyaml = i18nyaml
           config.boilerplate = boilerplate
         end
+      end
 
+      it "uses configuration options for organization and namespace" do
         FileUtils.rm_f "test.err.html"
         expect(xmlpp(strip_guid(Asciidoctor.convert(input, *OPTIONS))))
           .to(be_equivalent_to(xmlpp(output)))
@@ -491,24 +493,26 @@ RSpec.describe Metanorma::Generic do
           .to(be_equivalent_to(xmlpp(output)))
       end
 
-      Metanorma::Generic.configure do |config|
-        config.organization_name_short = Metanorma::Generic::Configuration.new.organization_name_short
-        config.organization_name_long = Metanorma::Generic::Configuration.new.organization_name_long
-        config.document_namespace = Metanorma::Generic::Configuration.new.document_namespace
-        config.docid_template = Metanorma::Generic::Configuration.new.docid_template
-        config.metadata_extensions = Metanorma::Generic::Configuration.new.metadata_extensions
-        config.stage_abbreviations = Metanorma::Generic::Configuration.new.stage_abbreviations
-        config.doctypes = Metanorma::Generic::Configuration.new.doctypes
-        config.default_doctype = Metanorma::Generic::Configuration.new.default_doctype
-        config.default_stage = Metanorma::Generic::Configuration.new.default_stage
-        config.termsdefs_titles = Metanorma::Generic::Configuration.new.termsdefs_titles
-        config.symbols_titles = Metanorma::Generic::Configuration.new.symbols_titles
-        config.normref_titles = Metanorma::Generic::Configuration.new.normref_titles
-        config.bibliography_titles = Metanorma::Generic::Configuration.new.bibliography_titles
-        config.committees = Metanorma::Generic::Configuration.new.committees
-        config.relations = Metanorma::Generic::Configuration.new.relations
-        config.i18nyaml = Metanorma::Generic::Configuration.new.i18nyaml
-        config.boilerplate = Metanorma::Generic::Configuration.new.boilerplate
+      after do
+        Metanorma::Generic.configure do |config|
+          config.organization_name_short = Metanorma::Generic::Configuration.new.organization_name_short
+          config.organization_name_long = Metanorma::Generic::Configuration.new.organization_name_long
+          config.document_namespace = Metanorma::Generic::Configuration.new.document_namespace
+          config.docid_template = Metanorma::Generic::Configuration.new.docid_template
+          config.metadata_extensions = Metanorma::Generic::Configuration.new.metadata_extensions
+          config.stage_abbreviations = Metanorma::Generic::Configuration.new.stage_abbreviations
+          config.doctypes = Metanorma::Generic::Configuration.new.doctypes
+          config.default_doctype = Metanorma::Generic::Configuration.new.default_doctype
+          config.default_stage = Metanorma::Generic::Configuration.new.default_stage
+          config.termsdefs_titles = Metanorma::Generic::Configuration.new.termsdefs_titles
+          config.symbols_titles = Metanorma::Generic::Configuration.new.symbols_titles
+          config.normref_titles = Metanorma::Generic::Configuration.new.normref_titles
+          config.bibliography_titles = Metanorma::Generic::Configuration.new.bibliography_titles
+          config.committees = Metanorma::Generic::Configuration.new.committees
+          config.relations = Metanorma::Generic::Configuration.new.relations
+          config.i18nyaml = Metanorma::Generic::Configuration.new.i18nyaml
+          config.boilerplate = Metanorma::Generic::Configuration.new.boilerplate
+        end
       end
     end
   end
