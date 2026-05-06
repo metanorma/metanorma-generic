67f085cfe0fe1d62243e3cf0f85ecafb35a27d01
Nick Nicholas <opoudjis@gmail.com>
2026-05-07 01:37:02 +1000

    rspec

--- spec/metanorma/validate_spec.rb
+++ spec/metanorma/validate_spec.rb
@@ -1,28 +1,6 @@
 require "spec_helper"
-require "metanorma"
 
 RSpec.describe Metanorma::Generic do
-  context "when xref_error.adoc compilation" do
-    around do |example|
-      FileUtils.rm_f "spec/assets/xref_error.err.html"
-      example.run
-      Dir["spec/assets/xref_error*"].each do |file|
-        next if file.match?(/adoc$/)
-
-        FileUtils.rm_f(file)
-      end
-    end
-
-    it "generates error file" do
-      expect do
-        Metanorma::Compile
-          .new
-          .compile("spec/assets/xref_error.adoc", type: "generic", "agree-to-terms": true)
-      end.to(change { File.exist?("spec/assets/xref_error.err.html") }
-              .from(false).to(true))
-    end
-  end
-
   it "does not issue doctype warning if doctype not supplied" do
     Metanorma::Generic.configure do |config|
       config.default_doctype = "pizza"
